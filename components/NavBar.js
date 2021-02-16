4b8918339b983664b66878599a449b285e30098a
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-16 16:21:49 -0500

    add non-mobile button & link to google form

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
-// import css from './navbar.module.css'
+import css from './navbar.module.css'
 
 const NavBar = () => {
     return ( 
-        <>
-            NavBar
-        </>
+        <div className={css.navbar}>
+            <div className={css.wrapper}>
+                <div className={css.mentor}>
+                    <a href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true">Become a Mentor</a>
+                </div>
+                <div>
+                    <button className={css.button}>
+                        Come Join Us!
+                    </button>
+                </div>
+            </div>
+        </div>
      );
 }
  
