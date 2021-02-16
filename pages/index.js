f63f07c430ec4fa85af77e471dc9537bd196a0db
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-16 17:56:54 -0500

    import components & comment out create-next-app code

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import NavBar from '../components/NavBar'
+import Intro from '../components/Intro'
+import SoundFamiliar from '../components/SoundFamiliar'
+import LearnByBreaking from '../components/LearnByBreaking'
+import WhatWeAreDoing from '../components/WhatWeAreDoing'
+import GetInvolved from '../components/GetInvolved'
+import ImageVisuals from '../components/ImageVisuals'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
+      <NavBar />
+      <Intro />
+      <SoundFamiliar />
+      <LearnByBreaking />
+      <WhatWeAreDoing />
+      <GetInvolved />
+      <ImageVisuals />
+      {/* <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -18,7 +28,8 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+        
+        
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
@@ -48,9 +59,21 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
+       
+        {/* <img src='/images/black-red.svg' alt='regular image tag' /> */}
+        {/* <Image src="/images/black-red.svg" alt="Image component from Next" width="64" height="64" /> */}
+        {/* <Image 
+        style={{
+        position: "relative",
+        width: "200px",
+        height: "200px",
+        maxHeight: "200px",
+        maxWidth: "200px",
+        }} 
+        src="/images/black-red.svg" alt="Image component from Next" layout="fill" /> */}
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -59,7 +82,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
