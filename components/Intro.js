fbb846493d3d3b231912bb96f20df637adc71d81
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-16 17:56:13 -0500

    first pass at intro section - mobile version

--- components/Intro.js
+++ components/Intro.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import css from './intro.module.css'
 
 const Intro = () => {
     return ( 
-        <h1> Intro </h1>
+            <div className={css.intro}>
+                <img className={css.logoimage} src='/images/coopLogo.svg' alt='The Coop Logo, where the word "Coop" is a stylized avian with glasses' />
+                <p className={css.mission}> is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</p>
+                <p >In addition to building real products, theCOOP aims to provide mentorship, resources, and networking opportunities for individual growth.</p>
+                <img className={css.workingimage} src='/images/hero-image.svg' alt='a chicken sits in a coup cum desk, their face illuminated by the laptop they are working on' />
+                <button className={css.button}> Learn More </button>
+            </div>
      );
 }
  
