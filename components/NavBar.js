65df5b9aaf6e53d4550c2a4238d094bea578f9e5
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-16 15:25:44 -0500

    set up navbar component

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import css from './navbar.module.css'
+
+const NavBar = () => {
+    return ( 
+        <>
+            NavBar
+        </>
+     );
+}
+ 
+export default NavBar;
+
+
