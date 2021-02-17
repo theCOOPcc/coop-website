2439a364ca0ed00c3cbc615d54770a558ddcea2b
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-17 11:51:22 -0500

    add all assets to the page at the bottom to aid in design

---
+++ components/ImageVisuals.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ImageVisuals = () => {
+    return ( 
+        <>
+        <img src='/images/temp/data-scientist-lockup.png' alt='data sci lockup png' />
+        <img src='/images/temp/data-scientist-lockup.svg' alt='data sci lockup svg' />
+        <img src='/images/temp/designers-lockup.svg' alt='designers lockup svg' />
+        <img src='/images/temp/Designers.png' alt='designers png' />
+        <img src='/images/temp/engineers-lockup.png' alt='engineers lockup png' />
+        <img src='/images/temp/engineers-lockup.svg' alt='engineers lockup svg' />
+        <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
+        <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
+        <img src='/images/temp/tile-group.svg' alt='tile group svg' />
+
+        <img src='/images/black-red.svg' alt='black-red' />
+        <img src='/images/coopLogo.png' alt='coop logo' />
+        <img src='/images/coopLogo.svg' alt='coop logosvg' />
+        <img src='/images/egg-jobseeker.svg' alt='egg jobseeker' />
+        <img src='/images/favicon.png' alt='favicon' />
+        <img src='/images/hero-image.svg' alt='hero image' />
+        <img src='/images/hiring.svg' alt='hiring' />
+        <img src='/images/par-break.svg' alt='par-break' />
+        <img src='/images/theCOOP-linkedIN.jpg' alt='theCOOP-linkedIN.jpg' />
+        
+
+
+
+        </>
+     );
+}
+ 
+export default ImageVisuals;
