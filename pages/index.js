d30f51f46998ad11070a1f73d170455c20a94d87
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-26 14:27:25 -0500

    get rid of <main> from vercel CNA

--- pages/index.js
+++ pages/index.js
@@ -32,61 +32,7 @@ export default function Home() {
       <WhatWeAreDoing />
       <GetInvolved />
       <ImageVisuals />
-    < /main>
-      {/* <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-        
-        
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main> */}
-       
-        {/* <img src='/images/black-red.svg' alt='regular image tag' /> */}
-        {/* <Image src="/images/black-red.svg" alt="Image component from Next" width="64" height="64" /> */}
-        {/* <Image 
-        style={{
-        position: "relative",
-        width: "200px",
-        height: "200px",
-        maxHeight: "200px",
-        maxWidth: "200px",
-        }} 
-        src="/images/black-red.svg" alt="Image component from Next" layout="fill" /> */}
-
+    </main>
       {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -97,7 +43,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer> */}
-    </div>
     </>
   )
 }
