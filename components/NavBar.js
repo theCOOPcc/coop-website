75752a1c350e4e0ec5d6e63a4f05238959fc1bd4
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-02-26 14:42:36 -0500

    update css import, mess around with different navbar code

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import bigCss from '../styles/Home.module.css'
 import css from './navbar.module.css'
 import styles from '../styles/Main.module.css'
 
 const NavBar = () => {
     return ( 
-        // <div className={css.navbar}>
-        //     <div className={css.wrapper}>
-        //         <div className={css.mentor}>
-        //             <a href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true">Become a Mentor</a>
-        //         </div>
-        //         <div>
-        //             <a href="https://docs.google.com/forms/d/1LXrZbFKELeey-CYQ2lRoM04PIfXK4faAXObg45jVxyc/edit">
-        //             <button className={css.button}>
-        //                 Come Join Us!
-        //             </button>
-        //             </a>
-        //         </div>
-        //     </div>
-        // </div>
+        <div className={css.navbar}>
+            <div className={css.wrapper}>
+                <div className={css.mentor}>
+                    <a href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true">Become a Mentor</a>
+                </div>
+                <div>
+                    <a href="https://docs.google.com/forms/d/1LXrZbFKELeey-CYQ2lRoM04PIfXK4faAXObg45jVxyc/edit">
+                    <button className={css.button}>
+                        Come Join Us!
+                    </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        // html website code
         // <nav>
         //     <ul>
         //         <a href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true" target="_blank">
@@ -29,20 +30,24 @@ const NavBar = () => {
         //         </a>    
         //     </ul>
         // </nav>
-        <nav>
-            <ul>
-                <li>
-                <a className={bigCss.tryIt} href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true" target="_blank">
-                    Become A Mentor
-                </a>
-                </li>
-                <li>
-                <a href="https://docs.google.com/forms/d/1LXrZbFKELeey-CYQ2lRoM04PIfXK4faAXObg45jVxyc/edit" target="_blank" >
-                    <button  className={css.joinUs}>Come Join Us!</button>
-                </a>
-                </li>    
-            </ul>
-        </nav>
+        
+        
+        
+        // Probably best nav bar option
+        // <nav>
+        //     <ul>
+        //         <li>
+        //         <a href="https://docs.google.com/forms/d/1GWvk2TqNZ5lU7oPqxFcldxlcVW8CUsY2-0-qoxGsfY4/viewform?edit_requested=true" target="_blank">
+        //             Become A Mentor
+        //         </a>
+        //         </li>
+        //         <li>
+        //         <a href="https://docs.google.com/forms/d/1LXrZbFKELeey-CYQ2lRoM04PIfXK4faAXObg45jVxyc/edit" target="_blank" >
+        //             <button  className={css.joinUs}>Come Join Us!</button>
+        //         </a>
+        //         </li>    
+        //     </ul>
+        // </nav>
      );
 }
  
