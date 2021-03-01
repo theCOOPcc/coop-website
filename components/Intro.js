1608a0b94daba356eba7373a761b237bfa2b5dfd
aaalexandriaaa <alexandria.r.stephenson@gmail.com>
2021-03-01 15:33:02 -0500

    halfway through working out intro refactor

--- components/Intro.js
+++ components/Intro.js
@@ -7,9 +7,11 @@ const Intro = () => {
             <div 
             className={styles.intro}
             >
-                <img 
+                <h1>
+                    <img 
                 // className={css.logoimage} 
-                src='/images/coopLogo.svg' alt='The Coop Logo, where the word "Coop" is a stylized avian with glasses' />
+                src='/images/coopLogo.svg' alt='The Coop' />
+                </h1>
                 <p 
                 // className={css.mission}
                 > is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</p>
@@ -28,13 +30,19 @@ export default Intro
 
 
 
-// <div class="intro"> 
+// <article class="intro"> 
 //     <div class="slide-container">
 //         <div class="flex-wrapper">                    
-//             <div class="main-message">
+//             <section class="main-message">
 //                 <img class="logo padding-bot-20" src="./resources/coopLogo.svg" alt="The Coop Logo"></img>
-//                 <div class="padding-bot-20"><h1>is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</h1></div>
+//  <h1 role="hidden"> The Coop </h1>
+
+//                 <div class="padding-bot-20">
+{/* <h1>is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</h1>
+</div> */}
+
 //                 <div class="padding-bot-20"><h2>In addition to building real products, theCOOP aims to provide mentorship, resources, and networking opportunities for individual growth.</h2></div>
+
 //                 <a href="mailto: info@thecoop.cc" target="_blank"><button class="learn-more">Learn More </button></a>
 //             </div>                        
 //             <div class="hero-image">
