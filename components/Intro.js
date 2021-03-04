10a4a8d29abef6b2fc51a558ee59f32afea41065
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2021-03-04 13:13:41 -0500

    intro section up-to-date for large screens

--- components/Intro.js
+++ components/Intro.js
@@ -3,51 +3,25 @@ import css from './intro.module.css'
 import styles from '../styles/Main.module.css'
 
 const Intro = () => {
-    return ( 
-            <div 
-            className={styles.intro}
-            >
-                <h1>
-                    <img 
-                // className={css.logoimage} 
-                src='/images/coopLogo.svg' alt='The Coop' />
-                </h1>
-                <p 
-                // className={css.mission}
-                > is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</p>
-                <p >In addition to building real products, theCOOP aims to provide mentorship, resources, and networking opportunities for individual growth.</p>
-                <img 
-                // className={css.workingimage} 
-                src='/images/hero-image.svg' alt='a chicken sits in a coup cum desk, their face illuminated by the laptop they are working on' />
-                <button 
-                // className={css.button}
-                > Learn More </button>
+    return (
+        <>
+            <div className={styles.intro} role="presentation/none"> 
+                <div className={styles.slideContainer} role="presentation/none">
+                    <div className={styles.flexWrapper} role="presentation/none">
+                        <section className={styles.mainMessage}>
+                            <h1><img className={`${styles.logo} ${styles.paddingBot20}`} src='/images/coopLogo.svg' alt='The Coop' /></h1>
+                            <div className={styles.paddingBot20} role="presentation/none"><h2>is an accelerator program focused on helping bootcamp graduates break in to the engineering and design field <span role="presentation/none" className={styles.pinkText}>faster</span> by providing them with real-world experience.</h2></div>
+                            <div className={styles.paddingBot20} role="presentation/none"><h3>In addition to building real products, theCOOP aims to provide mentorship, resources, and networking opportunities for individual growth.</h3></div>
+                            <a href="mailto: info@thecoop.cc" target="_blank"><button className={styles.learnMore}>Learn More </button></a>
+                        </section>
+                        <div className={styles.heroImage} role="presentation/none">
+                            <img src='/images/hero-image.svg' alt='a chicken sits in a coup cum desk, their face illuminated by the laptop they are working on' />
+                        </div>
+                    </div>
+                </div>
             </div>
+        </>
      );
 }
  
 export default Intro
-
-
-
-// <article class="intro"> 
-//     <div class="slide-container">
-//         <div class="flex-wrapper">                    
-//             <section class="main-message">
-//                 <img class="logo padding-bot-20" src="./resources/coopLogo.svg" alt="The Coop Logo"></img>
-//  <h1 role="hidden"> The Coop </h1>
-
-//                 <div class="padding-bot-20">
-{/* <h1>is a collective intent on helping career changers accelerate their transition into the Engineering and Design fields by providing them with real-world experience.</h1>
-</div> */}
-
-//                 <div class="padding-bot-20"><h2>In addition to building real products, theCOOP aims to provide mentorship, resources, and networking opportunities for individual growth.</h2></div>
-
-//                 <a href="mailto: info@thecoop.cc" target="_blank"><button class="learn-more">Learn More </button></a>
-//             </div>                        
-//             <div class="hero-image">
-//                 <img src="./resources/hero-image.svg" alt="theCOOP">
-//             </div>                  
-//         </div>
-//     </div>
-// </div>
