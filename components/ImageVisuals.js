6b5b430dc4bb6c1c8210761955e20c4a2aad2746
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2021-03-04 13:53:23 -0500

    add new images & update image assets component

--- components/ImageVisuals.js
+++ components/ImageVisuals.js
@@ -3,29 +3,37 @@ import React from 'react';
 const ImageVisuals = () => {
     return ( 
         <>
-        <img src='/images/temp/data-scientist-lockup.png' alt='data sci lockup png' />
-        <img src='/images/temp/data-scientist-lockup.svg' alt='data sci lockup svg' />
-        <img src='/images/temp/designers-lockup.svg' alt='designers lockup svg' />
-        <img src='/images/temp/Designers.png' alt='designers png' />
-        <img src='/images/temp/engineers-lockup.png' alt='engineers lockup png' />
-        <img src='/images/temp/engineers-lockup.svg' alt='engineers lockup svg' />
-        <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
-        <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
-        <img src='/images/temp/tile-group.svg' alt='tile group svg' />
+            <img src='/images/temp/data-scientist-lockup.png' alt='data sci lockup png' />
+            <img src='/images/temp/data-scientist-lockup.svg' alt='data sci lockup svg' />
+            <img src='/images/temp/designers-lockup.svg' alt='designers lockup svg' />
+            <img src='/images/temp/Designers.png' alt='designers png' />
+            <img src='/images/temp/engineers-lockup.png' alt='engineers lockup png' />
+            <img src='/images/temp/engineers-lockup.svg' alt='engineers lockup svg' />
+            <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
+            <img src='/images/temp/mentors-lockup.png' alt='mentors lockup png' />
+            <img src='/images/temp/tile-group.svg' alt='tile group svg' />
 
-        <img src='/images/black-red.svg' alt='black-red' />
-        <img src='/images/coopLogo.png' alt='coop logo' />
-        <img src='/images/coopLogo.svg' alt='coop logosvg' />
-        <img src='/images/egg-jobseeker.svg' alt='egg jobseeker' />
-        <img src='/images/favicon.png' alt='favicon' />
-        <img src='/images/hero-image.svg' alt='hero image' />
-        <img src='/images/hiring.svg' alt='hiring' />
-        <img src='/images/par-break.svg' alt='par-break' />
-        <img src='/images/theCOOP-linkedIN.jpg' alt='theCOOP-linkedIN.jpg' />
-        
-
-
-
+            <img src='/images/black-red.svg' alt='black-red' />
+            <img src='/images/coopLogo.png' alt='coop logo' />
+            <img src='/images/coopLogo.svg' alt='coop logosvg' />
+            <img src='/images/egg-jobseeker.svg' alt='egg jobseeker' />
+            <img src='/images/favicon.png' alt='favicon' />
+            <img src='/images/hero-image.svg' alt='hero image' />
+            <img src='/images/hiring.svg' alt='hiring' />
+            <img src='/images/par-break.svg' alt='par-break' />
+            <img src='/images/theCOOP-linkedIN.jpg' alt='theCOOP-linkedIN.jpg' />
+            
+            {/* New Images, 03/04/2021 */}
+            <img src='/images/caitlin.png' alt='caitlin.png' />
+            <img src='/images/candidate__quote.svg' alt='candidate__quote.svg' />
+            <img src='/images/Dan.png' alt='Dan' />
+            <img src='/images/designer-icon.svg' alt='designer-icon' />
+            <img src='/images/ds-icon.svg' alt='ds-icon' />
+            <img src='/images/engineer-icon.svg' alt='engineer-icon' />
+            <img src='/images/hm_quote.svg' alt='hm_quote' />
+            <img src='/images/jon_photo.png' alt='jon_photo' />
+            <img src='/images/mentor-icon.svg' alt='mentor-icon' />
+            <img src='/images/quoteMarks.svg' alt='quote marks' />
         </>
      );
 }
